a5664c24cb243529622f30fc5ffc3a438e56f95b
DesiZafirova <desszafirowa@gmail.com>
2025-01-16 23:39:39 +0200

    dispatching actions and adjusting the useDispatch hook

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { cartSlice } from './cart-slice';
 
-configureStore({
+export const store = configureStore({
   reducer: {
     cart: cartSlice.reducer,
   },
 });
+
+// this type has to describe how dispatchING will work, or which kind of data will be involved with dispatching in the application
+export type AppDispatch = typeof store.dispatch;
