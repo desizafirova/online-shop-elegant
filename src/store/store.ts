3edfdafca09c5fcc38a09b374507c752abc40e08
DesiZafirova <desszafirowa@gmail.com>
2025-01-17 00:30:55 +0200

    creating a Type-Safe useSelector hook

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,3 +9,6 @@ export const store = configureStore({
 
 // this type has to describe how dispatchING will work, or which kind of data will be involved with dispatching in the application
 export type AppDispatch = typeof store.dispatch;
+
+// Therefore RootState now is a function type. We are actually interested in the return value of that function though, not in the overall function. And thankfully, TypeScript has a utility type - Return type(part of TS)
+export type RootState = ReturnType<typeof store.getState>;
